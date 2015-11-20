80ea0db8e6bbae50a125fa5bddcba09b315ad64a
murataka2015 <murat-aka@hotmail.com>
2015-11-20 17:14:54 +0000

    new template for ejs dafault.ejs

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ var express = require("express");
 var app = express();
 
 
+app.set('view engine: ','ejs');
+
+
 app.get("/", function(req,res){
     
-    res.send("<H1>Hello </H1>Express");
+   // res.send("<H1>Hello </H1>Express");
+   res.render('default');
     
 
 });
